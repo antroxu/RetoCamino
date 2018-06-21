d963bfd68e2586e515691e1b0a7ef47dde090c88
Dev1 <Dev1@GET12-15.local>
2018-06-21 13:56:57 +0200

    reto-hitos page.work in progress

--- retoCaminoApp/src/pages/reto-hitos/reto-hitos.ts
+++ retoCaminoApp/src/pages/reto-hitos/reto-hitos.ts
@@ -13,8 +13,21 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'reto-hitos.html',
 })
 export class RetoHitosPage {
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for(let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
   }
 
   ionViewDidLoad() {

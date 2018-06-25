94d85ca2e71dffa2a135a8583ef82fe27d762b73
Dev1 <Dev1@GET12-15.local>
2018-06-25 13:02:16 +0200

    reto hitos page

--- retoCaminoApp/src/pages/reto-hitos/reto-hitos.ts
+++ retoCaminoApp/src/pages/reto-hitos/reto-hitos.ts
@@ -15,25 +15,40 @@ import { Storage } from '@ionic/storage';
 export class RetoHitosPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>; 
+  marcadores: any;
+  marcadores_listo: boolean;
 
   constructor(private storage: Storage) {
-    let marcadores: any;
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    //let marcadores: any;
+    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    // 'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    // this.items = [];
+    // for(let i = 1; i < 11; i++) {
+    //   this.items.push({
+    //     title: 'Item ' + i,
+    //     note: 'This is item #' + i,
+    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    //   });
+    // }
+
+    this.marcadores_listo = false;
 
     this.storage.get('marcadores').then(
       ok => {
-        marcadores = ok;
-        console.log('marcadores: ',marcadores);
+        this.marcadores = ok;
+        console.log('marcadores: ',this.marcadores);
+        this.marcadores_listo = true;
+        this.items =[]; 
+        for(let i = 0; i < this.marcadores.length; i++) {
+          console.log('el '+i+': ', this.marcadores[i].nombre);
+          this.items.push({
+            title: this.marcadores[i].nombre,
+            note: this.marcadores[i].direccion,
+            icon: this.marcadores[i].foto
+          });
+          console.log('parada')
+        }
       })
   }
 
